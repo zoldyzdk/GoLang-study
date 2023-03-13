b5d2103d35633fc1ee86c977727d6317e7fa2e7d
Hewerton <hewertonlourenco@gmail.com>
2023-03-13 16:39:57 -0300

    Feat: making a simple error handling in Go

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+  "log"
 
 	"example.com/greetings"
 )
 
 func main() {
-  //Get a freeting message and print it
-  message := greetings.Hello("Hewerton")
+  // Set properties of the predefined Logger, including
+  // the log entry prefix and a flag to disable printing
+  // the time, source file, and line number.
+  log.SetPrefix("greetings: ")
+  log.SetFlags(0)
+
+  //Request a greeting message.
+  message, err := greetings.Hello("")
+  // if an error was returned, print it to the console and
+  // exit the program.
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // if no error was returned, print the returned message
+  // to the console
   fmt.Println(message)
+
 }
